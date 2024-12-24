e947989909762ee80d58044d54e5bda3f5a9a15b
DjangO1107 <efremov.world@gmail.com>
2024-12-24 12:31:19 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,13 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2*2+2 == 2*(2+2))
+
+#4th program
+a=float(123.456)
+a=int(a*10)
+print(a%10//1)
